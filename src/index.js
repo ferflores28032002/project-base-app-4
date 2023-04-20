af4b7380b42238e8b613e11e1954d5a101003e90
fernando <fernando28032002@gmail.com>
2023-04-19 21:36:44 -0600

    se modifico el index

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { PORT } from "./config/envConfig.js";
 
 const app = express()
 // Routes 
-app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.use(cors())
 
 app.use('/api/', routeUser)
